f7a011bc058c40fe26ca5e4149be874b640edb00
Laura Sangeao Fontán <lsangeao@uoc.edu>
2024-04-20 19:40:28 +0200

    first project poetry

--- checkmailauthenticationtool/check_dmarc.py
+++ checkmailauthenticationtool/check_dmarc.py
@@ -3,6 +3,20 @@ import json
 
 
 def query_dns(domain,selector=''):
+    """
+    Queries DNS records for a given domain and returns information about DMARC, SPF, and DKIM records.
+
+    Args:
+        domain (str): The domain to query DNS records for.
+        selector (str, optional): The DKIM selector (if applicable). Defaults to ''.
+
+    Returns:
+        dict: A dictionary containing the following information:
+            - 'domain': The queried domain.
+            - 'dmarc': A list with the count of DMARC records found and a comma-separated string of DMARC records.
+            - 'spf': A list with the count of SPF records found and a comma-separated string of SPF records.
+            - 'dkim': A list with the count of DKIM records found and a comma-separated string of DKIM records.
+    """
     try:
         dmarc = [ str(x).split('"')[1] for x in dnsquery.resolve('_dmarc.' + domain , 'TXT').rrset if 'v=DMARC1' in str(x)]
 
@@ -29,9 +43,11 @@ def query_dns(domain,selector=''):
     
     return(result)
     
-r_domains=[]
-r_domains.append(query_dns('uoc.com','mail'))
-r_domains.append(query_dns('uned.es'))
 
-print(json.dumps(r_domains, indent=4))
+def main():   
+    r_domains=[]
+    r_domains.append(query_dns('uoc.com','mail'))
+    r_domains.append(query_dns('uned.es'))
+
+    print(json.dumps(r_domains, indent=4))
 
